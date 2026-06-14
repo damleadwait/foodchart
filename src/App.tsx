180bffa70b9868a430b379bfed670223ee362247
Adwait Damle <damleadwait@gmail.com>
2026-06-14 12:36:36 +0530

    Add weekly meal planner grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
+import "./App.css";
+
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>FoodChart</h1>
       <p>Weekly Meal Planner</p>
+
+      <table className="meal-table">
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Breakfast</th>
+            <th>Lunch</th>
+            <th>Dinner</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {days.map((day) => (
+            <tr key={day}>
+              <td>{day}</td>
+
+              <td>
+                <button>+ Add</button>
+              </td>
+
+              <td>
+                <button>+ Add</button>
+              </td>
+
+              <td>
+                <button>+ Add</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
